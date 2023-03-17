8fb4725e5c82f036436c61e25f49b131fb60af96
enyinnaya-joshua <enyinnayajoshua56@gmail.com>
2023-03-17 07:33:28 +0100

    saved all

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Properties from "./components/Properties/Propeties";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <Properties />
     </div>
   );
 }
