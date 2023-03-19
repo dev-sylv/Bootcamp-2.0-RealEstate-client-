114058bcdeedfdeff54f3320de8280a07a7986f7
nicsylvia <nicsylvia15f@gmail.com>
2023-03-18 20:40:50 -0700

    working on footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,137 +2,103 @@ import React from "react";
 import styled from "styled-components";
 import { BiBadgeCheck } from "react-icons/bi";
 import { GiCheckMark } from "react-icons/gi";
-import { AiFillLinkedin, AiFillTwitterCircle, AiOutlineFacebook, AiFillInstagram } from "react-icons/ai";
-
+import {
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+  AiOutlineFacebook,
+  AiFillInstagram,
+} from "react-icons/ai";
 
 const Footer = () => {
+  return (
+    <>
+      <Choice>
+        <Contain>
+          <Buyers>
+            <Icon>Customers</Icon>
 
-    return (
-        <>
+            <Max>Home</Max>
+            <Max>Agents</Max>
+            <Max>Buy</Max>
+            <Max>Rent</Max>
+          </Buyers>
 
-            <Choice>
+          <Buyers>
+            <Icon>About</Icon>
 
-                <Contain>
+            <Max>About Us</Max>
+            <Max>Contact Us</Max>
+            <Max>Services</Max>
+            <Max>Promotion</Max>
+          </Buyers>
 
-                            <Buyers>
-                                <Icon>
-                                    Customers
-                                </Icon> 
+          <Buyers>
+            <Icon>Legal</Icon>
 
-                                <Max>Home</Max>
-                                <Max>Agents</Max>
-                                <Max>Buy</Max>
-                                <Max>Rent</Max>
-                            </Buyers>
+            <Max>Terms & Conditions</Max>
+            <Max>FAQs</Max>
+            <Max>C of O</Max>
+            <Max>Legal Experts</Max>
+            <Max></Max>
+          </Buyers>
 
-                            <Buyers>
-                                <Icon>
-                                    About
-                                </Icon> 
+          <Buyers>
+            <Icon>Socials</Icon>
 
-                                <Max>About Us</Max>
-                                <Max>Contact Us</Max>
-                                <Max>Services</Max>
-                                <Max>Promotion</Max>
-                            </Buyers>
-
-                            <Buyers>
-                                <Icon>
-                                    Legal
-                                </Icon> 
-
-                                <Max>Terms & Conditions</Max>
-                                <Max>FAQs</Max>
-                                <Max>C of O</Max>
-                                <Max>Legal Experts</Max>
-                                <Max></Max>
-                            </Buyers>
-
-                            
-
-                            <Buyers>
-                                <Icon>
-                                    Socials
-                                </Icon> 
-
-                                <Max style={{display:"flex", fontSize:"25px", gap:"0.5rem"}}>
-                                    <AiOutlineFacebook />
-                                    <AiFillTwitterCircle />
-                                    <AiFillInstagram />
-                                    <AiFillLinkedin />
-                                </Max>
-                            </Buyers>
-
-                            
-
-                </Contain>
-
-            </Choice>
-
-        </>
-    )
-}
+            <Max style={{ display: "flex", fontSize: "25px", gap: "0.5rem" }}>
+              <AiOutlineFacebook />
+              <AiFillTwitterCircle />
+              <AiFillInstagram />
+              <AiFillLinkedin />
+            </Max>
+          </Buyers>
+        </Contain>
+      </Choice>
+    </>
+  );
+};
 
 export default Footer;
 
-// const Choice = styled.div``;
-
-// const Choice = styled.div``;
-
-// const Choice = styled.div``;
-
 const Max = styled.div`
-font-size: 14px;
-font-weight: 600;
-margin-top: 10px;
-color: #52325d;
-cursor: pointer;
+  font-size: 14px;
+  font-weight: 600;
+  margin-top: 10px;
+  color: #52325d;
+  cursor: pointer;
 `;
 
 const Icon = styled.div`
-font-size: 17px;
-font-weight: 700;
-color: #511378;
-margin-bottom: 15px;
+  font-size: 17px;
+  font-weight: 700;
+  color: #511378;
+  margin-bottom: 15px;
 `;
 
 const Buyers = styled.div`
-// width: 230px;
-// height: 230px;
-margin-right: 70px;
-margin-bottom: 50px;
-display: flex;
-flex-direction: column;
-// justify-content: center;
-// align-items: center;
+  margin-right: 70px;
+  margin-bottom: 50px;
+  display: flex;
+  flex-direction: column;
 `;
 
-const Left = styled.div`
-`;
-
-// const Bottom = styled.div`
-// display: flex;
-// justify-content: space-between;
-// flex-wrap: wrap;
-// margin-top: 20px;
-// `;
+const Left = styled.div``;
 
 const Contain = styled.div`
-width: 90%;
-display: flex;
-// flex-direction: column;
-flex-wrap: wrap;
-// justify-content: center;
-// align-items: center;
+  width: 90%;
+  display: flex;
+  // flex-direction: column;
+  flex-wrap: wrap;
+  // justify-content: center;
+  // align-items: center;
 `;
 
 const Choice = styled.div`
-width: 100%;
-display: flex;
-justify-content: center;
-align-items: center;
-padding-top: 50px;
-padding-bottom: 50px;
-overflow: hidden;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 50px;
+  padding-bottom: 50px;
+  overflow: hidden;
 `;
-
